695ab72e8f14063dbeee1886cab8f0c141fb9969
Roopa S <roopa31113@gmail.com>
2025-03-12 18:30:39 +0530

    Added styling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage/HomePage';
-import EventPage from './components/EventPage/EventPage';
+
 
 function App() {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<HomePage />} />
-                <Route path="/event" element={<EventPage />} />
             </Routes>
         </Router>
     );
--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,16 +1,13 @@
 import './HomePage.css';
 import couplePhoto from '../../assets/croppedcouple.png';
-import eventBg from '../../assets/swamy.jpg';
-import { useNavigate } from 'react-router-dom';
+import swamy from '../../assets/swamy.png';
 
 function HomePage() {
-    const navigate = useNavigate(); // Hook for navigation
-
     return (
         <div className="homepage-grid">
             {/* Hero Section */}
             <div className="hero-section">
-                <div className="tagline">Here's to 5 Amazing Years! 🎉❤️</div>
+                {/* <div className="tagline">Here's to 5 Amazing Years! 🎉❤️</div> */}
 
                 <div className="hero-container">
                     <img src={couplePhoto} alt="Couple" className="hero-image" />
@@ -25,13 +22,17 @@ function HomePage() {
 
             {/* Event Details Section */}
             <div className="event-section">
-                <h2 className="event-title">Satyanarayana Swamy Pooja & Anniversary Celebration</h2>
-                <p className="event-details">
-                    Join us for a special evening filled with divine blessings, love, and laughter.
-                </p>
-                <p className="event-date">📅 Date: March 25, 2025</p>
-                <p className="event-time">⏰ Time: 6:00 PM onwards</p>
-                <p className="event-venue">📍 Venue: Indikar Residence, Pune</p>
+                <img src={swamy} alt='swamy'/>
+                <h2 className="event-title">Sri Satyanarayana Swamy Pooja</h2>
+                
+                <div className="event-details-container">
+                    <p className="event-details">
+                        Join us for a special evening filled with divine blessings, love, and laughter.
+                    </p>
+                    <p className="event-date">📅 Date: March 25, 2025</p>
+                    <p className="event-time">⏰ Time: 6:00 PM onwards</p>
+                    <p className="event-venue">📍 Venue: Indikar Residence, Pune</p>
+                </div>
             </div>
         </div>
     );
