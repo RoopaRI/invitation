130abb0077b35564ed6bb3977425da000c147425
Roopa S <roopa31113@gmail.com>
2025-03-25 14:22:56 +0530

    css changes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,10 +7,12 @@ const Footer = () => {
         <footer className="footer">
             <p className="footer-title">If you're lost, Call us!</p>
             <div className="contact-info">
-                <p>📞 Rohit: +91 9742467082</p>
-                <p>📞 Ravindra: +91 7892091719</p>
-                <p>📞 Janhvi: +91 8880552181</p>
-            </div>
+    
+    <p><span className="name">📞 Ravindra</span> <span>:</span> <span className="number">+91 7892091719</span></p>
+    <p><span className="name">📞 Janhvi</span> <span>:</span> <span className="number">+91 8880552181</span></p>
+    <p><span className="name">📞 Rohit</span> <span>:</span> <span className="number">+91 9742467082</span></p>
+</div>
+
         </footer>
     );
 };
